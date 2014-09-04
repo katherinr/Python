86fd8819b2271774a2f0118ca20a02034629fa2b
unknown <artenika29@gmail.com>
2014-09-04 14:47:27 +0400

    Signed-off-by: unknown <artenika29@gmail.com>

---
+++ Memory.py
@@ -0,0 +1,91 @@
+# implementation of card game - Memory
+
+import simplegui
+import random
+index1=-1
+index2=-1
+state=0
+cards=[]
+counter=0
+exposed=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+# helper function to initialize globals
+def new_game():
+    global cards,state,exposed,counter
+    state=0
+    counter=0
+    exposed=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    label.set_text("Turns="+str(counter))
+    cards=range(8)
+    random.shuffle(cards)
+    cards1=range(8)
+    random.shuffle(cards1)
+    cards.extend(cards1)
+    return cards   
+
+
+# define event handlers
+def mouseclick(pos):
+    # add game state logic here
+    global cards,exposed
+    global state
+    global index1,index2
+    global counter
+    index=pos[0]//50
+    if (exposed[index]!=1): 
+        if state == 0:
+            exposed[index] = 1   
+            index2 = index
+            state = 1
+        elif state == 1:
+            exposed[index] = 1
+            index1 = index
+            state = 2
+            counter+=1
+        elif state == 2:
+            exposed[index] = 1
+            if cards[index2] != cards[index1]:
+                exposed[index2] = 0
+                exposed[index1] = 0
+            index2 = index
+            state = 1
+            counter += 1
+    label.set_text("Turns="+str(counter))
+                      
+# cards are logically 50x100 pixels in size    
+def draw(canvas):
+    global cards,exposed,state
+    pos=[5,80]
+    pos_line_b=[25,0]
+    pos_line_end=[25,100]
+    del_pos_b=[0,0]
+    del_pos_e=[0,100]
+    for i in range(16):
+       if (exposed[i]==1):
+           current_card=str(cards[i])
+           canvas.draw_text(current_card, pos, 80, "White")
+       else:
+           canvas.draw_line(pos_line_b,pos_line_end,50,"Green")
+           canvas.draw_line(del_pos_b,del_pos_e,3,"Lime")
+       pos[0]+=50
+       pos_line_b[0]+=50
+       pos_line_end[0]+=50 
+       del_pos_b[0]+=50
+       del_pos_e[0]+=50
+            
+# create frame and add a button and labels
+frame = simplegui.create_frame("Memory", 800, 100)
+frame.add_button("Restart", new_game)
+
+label = frame.add_label("Turns=0")
+
+# register event handlers
+frame.set_mouseclick_handler(mouseclick)
+frame.set_draw_handler(draw)
+
+
+# get things rolling
+new_game()
+frame.start()
+
+
+# Always remember to review the grading rubric
---
+++ Stop_watch.py
@@ -0,0 +1,110 @@
+# template for "Stopwatch: The Game"
+import simplegui
+# define global variables
+
+message = "0:00.0"
+attempts = 0   # number of clicks of the stop button
+count = 0	   # number of successful clicks
+time = 0	   # the timer
+tries = "0/0"  # successful attempts/attempts
+check = False
+
+# define helper function format that converts time
+# in tenths of seconds into formatted string A:BC.D
+def format(t):
+    data="NULL"
+    if (t==0):
+        data="0:00.0"
+        
+    if (t<10):
+        data="0:00."+str(t)
+        
+    if (t<100 and t>=10):
+        data="0:0"+str(t/10)+"."+str(t%10)
+       
+    if (t>=100 and t<600):
+        data="0:"+str(t/10)+"."+str(t%10)
+         
+    if (t>=600 and t<1200):
+        t=t-600
+        if (t<100):
+            data="1:0"+str(t/10)+"."+str(t%10)    
+        if (t>100):
+            data="1:"+str(t)+"."+str(t%10)
+    if (t>=1200):
+       first_digit=str(t/600)
+       rem=t%600
+       if (rem==0):
+           data=first_digit+":00.0"
+            
+       if (rem<10):
+           data=first_digit+":00."+str(rem)
+            
+       if (rem<100 and rem>=10):
+           data=first_digit+":"+str(rem/10)+"."+str(t%10)
+       
+       if (rem>=100 and rem<600):
+           data=first_digit+":"+str(rem/10)+"."+str(t%10)
+                    
+    return data
+    
+# define event handlers for buttons; "Start", "Stop", "Reset"
+def start():
+    global check
+    timer.start()
+    check = True
+    
+
+def stop():
+    global tries, time, count, attempts, check
+    timer.stop()
+    if (check!=False):
+        attempts+=1
+        check = False
+        if (time%10==0):
+            count+=1
+    tries=str(count)+ "/" + str(attempts)
+    
+
+def reset():
+    global count, time, attempts, tries, message, check
+    check = False 
+    timer.stop()
+    tries = "0/0"
+    attempts = 0
+    count = 0
+    time = 0
+  
+def update():
+    global time, message, check
+    message=format(time)
+    return message   
+
+def timer_handler():
+    global time
+    time += 1
+    
+    
+# define event handler for timer with 0.1 sec interval
+ 
+timer = simplegui.create_timer( 100, timer_handler)
+
+# define draw handler
+def draw(canvas):
+    message=update()
+    canvas.draw_text(message, [100,150], 46, "White")
+    canvas.draw_text(tries, [240,30], 26, "Lime")
+    
+# create frame
+frame=simplegui.create_frame("Stopwatch_game",300,300)
+frame.set_draw_handler(draw)
+
+# register event handlers
+frame.add_button("Start",start, 100)
+frame.add_button("Stop", stop, 100)
+frame.add_button("Reset", reset, 100)
+
+# start frame
+frame.start()
+format(0)
+# Please remember to review the grading rubric
